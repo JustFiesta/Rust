55b0abe328d55cf871a2a5ea99cf2d57dbdb8bd7
JustFiesta <mateusz.bocak9@gmail.com>
2023-08-11 15:04:39 +0200

    Better naming - no warings, gitignore setted up

--- Hello_World.rs
+++ Hello_World.rs
@@ -2,8 +2,8 @@ fn main() {
     println!("Hello world!");
     let s = String::from("Hello world!");
     println!("{}", s);
-    let moveS = s + "How are you?"; //Moved ownership of s to moveS - we cant use s after this line
+    let move_s = s + "How are you?"; //Moved ownership of s to moveS - we cant use s after this line
 
     // println!("{}", s); - this print macro wont work
-    println!("{}", moveS);
+    println!("{}", move_s);
 }
